8b3064b323fb084d7ee16eaa89b05c34de0305e8
Efreak <efreak@efreakbnc.net>
2012-12-10 21:08:44 -0600

    eh, fuck it. Someone else cal deal with this shit, I don't need it any more and I'm too lazy

--- whois.cpp
+++ whois.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Efreak.
+ * Copyright (C) 2012 Efreak.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published
